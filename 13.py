a67e0b9fe37b0aa3c99e95f8e3d8465ebe4fee13
ksn <ksn0215@gmail.com>
2018-07-07 06:28:32 +0900

    a

---
+++ 13.py
@@ -0,0 +1,49 @@
+class Solution:
+   def romanToInt(self,s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        sum = 0
+        count_I = 0
+        count_X = 0
+        count_C = 0
+        for i in s:
+            if i == 'I':
+                count_I += 1
+                sum += 1
+            elif i == 'V':
+                if count_I == 1:
+                    sum += 3
+                else:
+                    sum += 5
+            elif i == 'X':
+                if count_I == 1:
+                    count_X += 1
+                    sum += 8
+                else:
+                    count_X += 1
+                    sum += 10
+            elif i == 'L':
+                if count_X == 1:
+                    sum += 30
+                else:
+                    sum += 50
+            elif i == 'C':
+                if count_X == 1:
+                    count_C += 1
+                    sum += 80
+                else:
+                    count_C += 1
+                    sum += 100
+            elif i == 'D':
+                if count_C == 1:
+                    sum += 300
+                else:
+                    sum += 500
+            elif i == 'M':
+                if count_C == 1:
+                    sum += 800
+                else:
+                    sum += 1000
+        return sum
