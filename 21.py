f5bc0b8c331a4ad5fcc59ebaf1b47c3f370922fe
ksn <ksn0215@gmail.com>
2018-07-10 05:35:04 +0900

    a

---
+++ 21.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if not l1: return l2
+        if not l2: return l1
+        current = head = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                current.next = l1
+                l1 = l1.next
+            else:
+                current.next = l2
+                l2 = l2.next
+            current = current.next
+        if l1:
+            current.next = l1
+        else:
+            current.next = l2
+        return head.next
