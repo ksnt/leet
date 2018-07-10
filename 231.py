9462b7b8c1c32d408f0fb6c5020a317c3819b9a6
ksn <ksn0215@gmail.com>
2018-07-11 01:09:25 +0900

    a

---
+++ 231.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPowerOfTwo(self,n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n == 0:
+            return False
+        elif n == 1:
+            return True
+        elif n % 2 == 0:
+            return self.isPowerOfTwo(n/2)
+        else:
+            return False
