d01e3f9a4623f7716649467ce1a034ece4e1a832
ksn <ksn0215@gmail.com>
2018-07-14 04:42:58 +0900

    a

--- 125.py
+++ 125.py
@@ -4,8 +4,5 @@ class Solution:
         :type s: str
         :rtype: bool
         """
-        a = re.sub(re.compile("[!-/:-@[-`{-~\s]"), '', s).replace(" ","").lower()
-        if len(a) % 2 == 0:
-            return a[0:len(a)//2] == a[::-1][0:len(a)//2]
-        else:
-            return a[0:len(a)//2] == a[::-1][0:len(a)//2]
+        a = re.sub(re.compile("[!-/:-@[-`{-~\s]"), '', s).lower()
+        return a[0:len(a)//2] == a[::-1][0:len(a)//2]
---
+++ 125_1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        a = re.sub(re.compile("[!-/:-@[-`{-~\s]"), '', s).replace(" ","").lower()
+        if len(a) % 2 == 0:
+            return a[0:len(a)//2] == a[::-1][0:len(a)//2]
+        else:
+            return a[0:len(a)//2] == a[::-1][0:len(a)//2]
