34730e8f6af74c1be5886ee74b1a8269cf134195
ksn <ksn0215@gmail.com>
2018-07-14 05:34:25 +0900

    a

---
+++ 229.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def majorityElement(self,nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        res = []
+        list_length = len(nums)
+        c = collections.Counter(nums)
+        for t in c.items():
+            if t[1] > (list_length / 3):
+                res.append(t[0])
+        return res
