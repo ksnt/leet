27f7a8028e4352434b1402c05e1483d396872a73
ksn <ksn0215@gmail.com>
2018-07-15 01:07:54 +0900

    a

--- 194.sh
+++ 194.sh
@@ -1,4 +1,5 @@
 # Read from the file file.txt and print its transposed content to stdout.
+# Reference: https://stackoverflow.com/questions/1729824/an-efficient-way-to-transpose-a-file-in-bash
 awk '
 { 
     for (i=1; i<=NF; i++)  {
