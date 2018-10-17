8986cc1a1c7395e0fc9bccd0788a4ed8d4798ca6
ksnt <ksn0215@gmail.com>
2018-10-18 07:57:53 +0900

    a

---
+++ 319_pochmann.py
@@ -0,0 +1,9 @@
+import math
+# solution by Stefan Pochmann 
+class Solution:
+    def bulbSwitch(self,n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return int(math.sqrt(n))
