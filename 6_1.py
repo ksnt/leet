c20a7d13d6249f51f3078e0d060f71d1c5af7435
ksnt <ksn0215@gmail.com>
2018-10-26 08:59:01 +0900

    a

---
+++ 6_1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def convert(self,s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if len(s) <= numRows or numRows==1:
+            return s
+        res = [[] for _ in range(numRows)]
+        mod_base = 2*numRows - 2
+        for i in range(len(s)):
+            j = i % mod_base
+            if j <= numRows -1:
+                res[j].append(s[i])
+            else:
+                res[mod_base - j].append(s[i])
+        #print(res)
+        return "".join(list(map("".join,res)))
